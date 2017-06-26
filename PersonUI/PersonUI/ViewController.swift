09c0c9fb093321e3d3945b3408b05957d988d3f1
Bruce Thomson <bwt5z6@mail.missouri.edu>
2017-06-26 17:46:59 -0500

    Added Xcode Files

---
+++ PersonUI/PersonUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonUI
+//
+//  Created by Bruce Thomson on 6/26/17.
+//  Copyright © 2017 Bruce Thomson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
